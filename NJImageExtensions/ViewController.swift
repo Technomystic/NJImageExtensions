41eec146f3e44e3fb9889a1a31521ab37652c453
Technomystic <lost.trust@yahoo.co.in>
2020-04-18 22:28:25 +0100

    Initial Commit

---
+++ NJImageExtensions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NJImageExtensions
+//
+//  Created by Niraj on 18/04/2020.
+//  Copyright © 2020 Technomystic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NJImageExtensionsTests/NJImageExtensionsTests.swift
@@ -0,0 +1,27 @@
+//
+//  NJImageExtensionsTests.swift
+//  NJImageExtensionsTests
+//
+//  Created by Niraj on 18/04/2020.
+//  Copyright © 2020 Technomystic. All rights reserved.
+//
+
+import XCTest
+@testable import NJImageExtensions
+
+class NJImageExtensionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+       let i = 0
+       XCTAssert(i == 0, "Value of i should be equal to 0")
+    }
+
+}
